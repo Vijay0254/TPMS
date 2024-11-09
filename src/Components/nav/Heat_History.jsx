e3ceefa300217723358a068036e0612aa207df76
Vijay0254 <vijayabijeet254@gmail.com>
2024-11-09 16:11:14 +0530

    Fourth Commit

--- src/Components/nav/Heat_History.jsx
+++ src/Components/nav/Heat_History.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Heat_History = () => {
   return (
-    <section className='grid place-items-center pt-20'>
+    <>
+      <h1 className='text-[#FFD272] text-center pt-10 font-bold text-4xl'>Heat/Cut Resistance History</h1>
+      <section className='grid place-items-center pt-5'>
         <h1 className='text-4xl font-bold text-white'>Not yet Updated...</h1>
-    </section>
+      </section>
+    </>
   )
 }
 
--- src/Components/nav/Speed_History.jsx
+++ src/Components/nav/Speed_History.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Speed_History = () => {
   return (
-    <section className='grid place-items-center pt-20'>
+    <>
+      <h1 className='text-[#FFD272] text-center pt-10 font-bold text-4xl'>Speed History</h1>
+      <section className='grid place-items-center pt-5'>
         <h1 className='text-4xl font-bold text-white'>Not yet Updated...</h1>
-    </section>
+      </section>
+    </>
   )
 }
 
